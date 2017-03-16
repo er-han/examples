0997bf2e51d62efef98f51b071124305e155216c
Erhan Karakaya <erhan277@gmail.com>
2017-03-16 15:37:52 +0300

    Added Application main class

---
+++ spring-examples/spring-data-jpa-extended/src/main/java/Application.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by erhan.karakaya on 3/16/2017.
+ */
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
