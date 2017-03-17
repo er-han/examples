bcb49a25b14365aec56140fd8fdbdd4d028d2750
Erhan Karakaya <erhan277@gmail.com>
2017-03-17 17:12:16 +0300

    Fixed save method in ProductService

--- spring-examples/spring-data-jpa-extended/src/main/java/tr/com/erhankarakaya/examples/jpa/bll/service/ProductServiceImpl.java
+++ spring-examples/spring-data-jpa-extended/src/main/java/tr/com/erhankarakaya/examples/jpa/bll/service/ProductServiceImpl.java
@@ -1,17 +1,32 @@
 package tr.com.erhankarakaya.examples.jpa.bll.service;
 
+import mapper.exception.ResultTypeInstantiationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import tr.com.erhankarakaya.examples.jpa.bll.dto.ProductDto;
+import tr.com.erhankarakaya.examples.jpa.dal.entity.Product;
+import tr.com.erhankarakaya.examples.jpa.dal.repository.ProductRepository;
 
 import java.util.List;
 
 
 @Service
 public class ProductServiceImpl implements ProductService {
+  @Autowired
+  ProductRepository productRepository;
+
   @Override
   public ProductDto save(ProductDto productDto) {
-    return null;
+    Assert.notNull(productDto);
+    try {
+      Product product = productDto.mapTo(Product.class);
+      product = productRepository.save(product);
+      return product.mapTo(ProductDto.class);
+    } catch (ResultTypeInstantiationException e) {
+      return null;
+    }
   }
 
   @Override
