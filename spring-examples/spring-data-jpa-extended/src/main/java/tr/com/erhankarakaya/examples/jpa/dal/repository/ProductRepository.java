a66d49db443705164f6af8c9d2ec3803fcf714a2
Erhan Karakaya <erhan277@gmail.com>
2017-03-17 10:27:35 +0300

    Added ProductRepository

---
+++ spring-examples/spring-data-jpa-extended/src/main/java/tr/com/erhankarakaya/examples/jpa/dal/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package tr.com.erhankarakaya.examples.jpa.dal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tr.com.erhankarakaya.examples.jpa.dal.entity.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+}
