43450267c90f6c002c0194caf8251bcb6096cbbf
Erhan Karakaya <erhan277@gmail.com>
2017-03-21 16:22:57 +0300

    Added WebConfig class extending WebMvcConfigurerAdapter
    Overrided addArgumentResolver to set default PagaRequest

---
+++ spring-examples/spring-data-jpa-extended/src/main/java/tr/com/erhankarakaya/examples/jpa/config/WebConfig.java
@@ -0,0 +1,25 @@
+package tr.com.erhankarakaya.examples.jpa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by erhan.karakaya on 3/21/2017.
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+    PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+    resolver.setOneIndexedParameters(true);
+    resolver.setFallbackPageable(new PageRequest(1, 50));
+    argumentResolvers.add(resolver);
+    super.addArgumentResolvers(argumentResolvers);
+  }
+}
