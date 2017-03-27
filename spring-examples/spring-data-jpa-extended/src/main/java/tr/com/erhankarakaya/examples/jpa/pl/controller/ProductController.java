2a3b9e766191a45bec6877163cbf6388d942277a
Erhan Karakaya <erhan277@gmail.com>
2017-03-27 11:29:02 +0300

    Added action method for saveProduct

--- spring-examples/spring-data-jpa-extended/src/main/java/tr/com/erhankarakaya/examples/jpa/pl/controller/ProductController.java
+++ spring-examples/spring-data-jpa-extended/src/main/java/tr/com/erhankarakaya/examples/jpa/pl/controller/ProductController.java
@@ -8,12 +8,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import tr.com.erhankarakaya.examples.jpa.bll.dto.BindingResultDto;
 import tr.com.erhankarakaya.examples.jpa.bll.dto.ProductDto;
 import tr.com.erhankarakaya.examples.jpa.bll.service.ProductService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -25,7 +27,7 @@ public class ProductController {
   @Autowired
   ProductService productService;
 
-  @RequestMapping("/getproducts")
+  @RequestMapping("/products")
   public String findAll(Pageable pageable, Model model) {
 
     Page<ProductDto> productDtos = productService.findAll(pageable);
@@ -33,6 +35,29 @@ public class ProductController {
     model.addAttribute("products", productDtos);
 
     return "products";
+  }
+
+
+  @RequestMapping(value = "/save", method = RequestMethod.GET)
+  public String saveGet(Model model) {
+
+      model.addAttribute("product", new ProductDto());
+      return "save";
+  }
+
+  @RequestMapping(value = "/save", method = RequestMethod.POST)
+  public String save(@Valid ProductDto productDto, BindingResult bindingResult, Model model) {
+
+    if (bindingResult.hasErrors()) {
+      BindingResultDto bindingResultDto = new BindingResultDto();
+      bindingResultDto.setAllErros(bindingResult.getAllErrors());
+      model.addAttribute("errors", bindingResultDto);
+      model.addAttribute("error", bindingResult);
+      return "save";
+    }
+    productDto = productService.save(productDto);
+
+    return "redirect:/products?" + bindingResult.hasErrors();
   }
 
 }
