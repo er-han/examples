96bef32634a927cb8645090061a3e197f465fbd8
Erhan Karakaya <erhan277@gmail.com>
2017-03-28 16:49:12 +0300

    Added product class

---
+++ design-pattern-examples/builder-pattern-example/src/main/java/seperate/Product.java
@@ -0,0 +1,63 @@
+package seperate;
+
+import java.time.LocalDate;
+
+/**
+ * Created by erhan.karakaya on 3/28/2017.
+ */
+public class Product {
+  private Integer id;
+  private String title;
+  private String description;
+  private LocalDate expirationDate;
+  private Double price;
+  private Integer quantity;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public LocalDate getExpirationDate() {
+    return expirationDate;
+  }
+
+  public void setExpirationDate(LocalDate expirationDate) {
+    this.expirationDate = expirationDate;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+}
